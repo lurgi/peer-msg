f90d3deb6933a9458864750b76a55bd900321ee2
Jeongwoo Park <wahoo9040@naver.com>
2025-05-12 12:37:56 +0900

    chore[library]: Rollup 빌드 시스템 설정

---
+++ library/rollup.config.js
@@ -0,0 +1,27 @@
+const typescript = require("@rollup/plugin-typescript");
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const pkg = require("./package.json");
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+module.exports = {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+  ],
+  external,
+  plugins: [resolve(), typescript(), commonjs()],
+};
